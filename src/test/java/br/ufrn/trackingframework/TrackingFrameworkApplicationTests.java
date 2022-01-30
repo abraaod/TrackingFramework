df2f4191b8b8cf909156bbe253ade1f7bbf35fe7
abraaod <abraaovld@gmail.com>
2022-01-30 15:39:30 -0300

    Setup project

---
+++ src/test/java/br/ufrn/trackingframework/TrackingFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.trackingframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
