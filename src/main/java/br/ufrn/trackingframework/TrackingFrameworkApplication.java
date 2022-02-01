5bbd86944a0d815a0a1c5b286249be78ca449173
abraaod <abraaovld@gmail.com>
2022-02-01 17:41:02 -0300

    Generic change

--- src/main/java/br/ufrn/trackingframework/TrackingFrameworkApplication.java
+++ src/main/java/br/ufrn/trackingframework/TrackingFrameworkApplication.java
@@ -2,13 +2,8 @@ package br.ufrn.trackingframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-        MongoAutoConfiguration.class,
-        MongoDataAutoConfiguration.class
-})
+@SpringBootApplication()
 public class TrackingFrameworkApplication {
 
     public static void main(String[] args) {
