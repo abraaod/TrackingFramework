511bb84efcf01534d3849ae2c72735a90a512ced
abraaod <abraaovld@gmail.com>
2022-02-01 17:37:37 -0300

    Updated mongo connection

--- src/main/java/br/ufrn/trackingframework/config/MongoConfig.java
+++ src/main/java/br/ufrn/trackingframework/config/MongoConfig.java
@@ -5,24 +5,25 @@ import com.mongodb.reactivestreams.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 
-@Configuration
-public class MongoConfig {
-
-    @Value("${app.extension.mongo.host}")
-    private String mongoHost;
-
-    @Value("${app.extension.mongo.database}")
-    private String mongoDatabase;
-
-    @Bean
-    public MongoClient reactiveMongoClient(){
-        return MongoClients.create(mongoHost);
-    }
-
-    @Bean
-    public ReactiveMongoTemplate reactiveMongoTemplate(){
-        return new ReactiveMongoTemplate(reactiveMongoClient(), mongoDatabase);
-    }
-}
+//@Configuration
+//public class MongoConfig {
+//
+//    @Value("${app.extension.mongo.host}")
+//    private String mongoHost;
+//
+//    @Value("${app.extension.mongo.database}")
+//    private String mongoDatabase;
+//
+//    @Bean
+//    public MongoClient reactiveMongoClient(){
+//        return MongoClients.create(mongoHost);
+//    }
+//
+//    @Bean
+//    public ReactiveMongoTemplate reactiveMongoTemplate() {
+//        return new ReactiveMongoTemplate(reactiveMongoClient(), mongoDatabase);
+//    }
+//}
