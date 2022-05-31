0787bc8d712f668d2c4980078a723321670f2ee8
abraaod <abraaovld@gmail.com>
2022-05-31 19:31:31 -0300

    Added CRUD methods to service

--- src/main/java/br/ufrn/trackingframework/service/DataEntityService.java
+++ src/main/java/br/ufrn/trackingframework/service/DataEntityService.java
@@ -5,6 +5,7 @@ import br.ufrn.trackingframework.repository.DataEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -18,9 +19,13 @@ public class DataEntityService {
         return repository.findAll();
     }
 
+    public Mono<DataEntity> getById(String id) { return repository.findById(id); }
+
     public Mono<DataEntity> save(DataEntity dataEntity){
         return repository.save(dataEntity);
     }
 
-    public Mono deleteAll() { return repository.deleteAll(); }
+    public Mono deleteById(String id) {
+        return repository.deleteById(id);
+    }
 }
