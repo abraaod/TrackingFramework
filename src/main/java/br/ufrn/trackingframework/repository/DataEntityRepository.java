2d2ecb49055dbb64bbb45046bbc447b1a6f6b65c
Abraão Dantas <abraaovld@gmail.com>
2022-10-10 16:50:37 -0300

    Removed unused dependency

--- src/main/java/br/ufrn/trackingframework/repository/DataEntityRepository.java
+++ src/main/java/br/ufrn/trackingframework/repository/DataEntityRepository.java
@@ -1,7 +1,6 @@
 package br.ufrn.trackingframework.repository;
 
 import br.ufrn.trackingframework.Model.DataEntity;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
